e9ebfb4f4fc8c271aa6b31de0c47ff481a014a68
Pablo Medina <pablomedina85@gmail.com>
2014-04-11 19:43:43 -0300

    KVStoreBootstrap - receive binding (local) instead of just the port

--- src/main/scala/ckite/example/KVStoreBootstrap.scala
+++ src/main/scala/ckite/example/KVStoreBootstrap.scala
@@ -3,9 +3,9 @@ import ckite.CKiteBuilder
 
 object KVStoreBootstrap extends App {
 
-  val localPort = System.getProperty("port")
+  val localBinding = System.getProperty("binding")
   val members = if (System.getProperty("members") != null)  System.getProperty("members").split(",").toSeq else Seq()
-  val ckite = CKiteBuilder().withLocalBinding(s"localhost:$localPort")
+  val ckite = CKiteBuilder().withLocalBinding(localBinding)
   								.withMembersBindings(members)
   								.withDataDir(System.getProperty("dataDir"))
   								.withStateMachine(new KVStore())
